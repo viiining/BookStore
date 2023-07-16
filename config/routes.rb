33b706cdadeb6df1359a8e6cc686420c76b451b2
Zinni Chang <czinni853@gmail.com>
2023-07-17 00:29:36 +0800

    revised the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'books#index' # 2. root 建立
   resources :books
-  # root 'books#index' # 2. root 建立
   # get '/books', to: 'books#index' # 1. /books 建立
 
   # get '/books/new', to: 'books#new' # 3. /books/new 建立
