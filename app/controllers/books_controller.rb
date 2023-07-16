1fa58fa583f0c858a605a2d95142759520106b65
Zinni Chang <czinni853@gmail.com>
2023-07-16 22:05:41 +0800

    new form and save in database and redirect to index.html

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,18 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    book_params = params.require(:book).permit(:bookname, :description, :price, :page)
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to "/books", notice: "書籍新增成功"
+    else
+      render :new
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'books#index' # 2. root 建立
-  get '/books', to: 'books#index' # 1. /books 建立
+  resources :books
+  # root 'books#index' # 2. root 建立
+  # get '/books', to: 'books#index' # 1. /books 建立
 
-  get '/books/new', to: 'books#new' # 3. /books/new 建立
-  post '/books', to: 'books#create'
+  # get '/books/new', to: 'books#new' # 3. /books/new 建立
+  # post '/books', to: 'books#create'
 end
