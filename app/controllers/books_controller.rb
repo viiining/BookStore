8fcdcfd45c420ac1a6a8b693d5bb761faad89876
Zinni Chang <czinni853@gmail.com>
2023-07-17 00:20:03 +0800

    add update function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,9 +18,24 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
 
     if @book.save
-      redirect_to "/books", notice: "書籍新增成功"
+      redirect_to books_path, notice: "書籍新增成功"
     else
       render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    book_params = params.require(:book).permit(:bookname, :description, :price, :page)
+    if @book.update(book_params)
+      redirect_to books_path, notice: "文章更新成功"
+    else
+      render :edit
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get '/books/new', to: 'books#new' # 3. /books/new 建立
   post '/books', to: 'books#create' # 4.
 
-  get '/books/:id', to: 'books#show', as: 'book'
+  get '/books/:id', to: 'books#show', as: 'book' # 5.
+
+  get '/books/:id/edit', to: 'books#edit', as: 'edit_book' # 6.
+  patch '/books/:id/', to: 'books#update'
 end
