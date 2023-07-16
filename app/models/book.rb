04228e4dfe447693c17b3f0602bde2c292778754
Zinni Chang <czinni853@gmail.com>
2023-07-16 22:06:28 +0800

    add validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+    validates :bookname, presence: true
+    validates :page, presence: true, numericality: { greater_than: 0 }
+    validates :price, presence: true, numericality: { greater_than: 0 }
 end
