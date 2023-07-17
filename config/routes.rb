4c4e0a1c95cc9b2fbee86308ea2e001de915bf5d
Zinni Chang <czinni853@gmail.com>
2023-07-17 23:18:32 +0800

    revised typo

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   # get '/books/:id', to: 'books#show', as: 'book' # 5.
 
   # get '/books/:id/edit', to: 'books#edit', as: 'edit_book' # 6.
-  # patch '/books/:id/', to: 'books#update'
+  # patch '/books/:id', to: 'books#update'
   # delete '/books/:id', to: 'books#destroy'
 end
